2c250bb33aee9da10c8f066855e26081e26f146d
Vitor Fernandes Moraes <80361373+Vitor-php@users.noreply.github.com>
2022-05-31 11:25:19 -0300

    controller sucessfull

---
+++ larafood/resources/views/Admin/pages/plans/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content-header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Lista de Planos</p>
+@stop
--- larafood/routes/web.php
+++ larafood/routes/web.php
@@ -1,8 +1,11 @@
+
 <?php
+
+use App\Http\Controllers\PlanController;
 
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
